9f48b9108000a3b925f6387834860ee5897bda5a
Trishium <pdhumphris@gmail.com>
2026-02-27 10:41:47 +0000

    Add cipher factory

---
+++ src/MPAGSCipher/CipherFactory.hpp
@@ -0,0 +1,12 @@
+#ifndef CIPHERFACTORY_HPP
+#define CIPHERFACTORY_HPP
+#include "Cipher.hpp"
+#include "CipherType.hpp"
+#include <memory>
+    
+
+namespace CipherFactory {
+std::unique_ptr<Cipher> makeCipher(const CipherType type, const std::string& key);
+
+}
+#endif // CIPHERFACTORY_HPP
